65ff4563e4c7d1e44a839f0dc92331098250fceb
nandemotoken <nandemotoken@gmail.com>
2020-05-31 12:23:35 +0000

    index.html

--- blockchain_hundler.js
+++ blockchain_hundler.js
@@ -1,7 +1,7 @@
+//スマートコントラクトのアドレスです。MetaMaskで確認できます。
 const CalcAddress = "0xE57195d40EB77017f25F3C8dE8f2bA5962AdC60E";
-//metamask利用許可ダイアログを出す
-ethereum.enable();
 
+//ブロックチェーン呼び出しのためのルールを記したABIというものです。Remixから拾います。
 const ABI = [
 {
     "constant": false,
@@ -34,3 +34,26 @@ const ABI = [
     "type": "function"
 }
 ]
+
+
+//--------------ここからメイン処理---------------
+//------web3.jsの読み込み待ちのため、window.onloadにメイン処理を入れる--------
+
+// ここにasyncと書いておくと、await(処理待ち)ができます。
+window.onload = async function () {
+
+    //metamask利用許可ダイアログを出す
+    await ethereum.enable();
+
+    //metamaskの準備
+    web3js = new Web3(web3.currentProvider);
+    //スマートコントラクト読み込み
+    mycontract = new web3js.eth.Contract(ABI, Address);
+  
+    //1.2.7と表示されるのが正しい
+    console.log("reader.js_is_checking_web3.js_version..." + Web3.version);
+  
+    //ブロックチェーンから数字を読み込みます。
+    let numberFromBlockchain = await mycontract.methods.getNumber().call()
+    document.getElementById("number").innerHTML = numberFromBlockchain;
+  }
